fee93301c85536352e21c902f3582c877ad7791f
Diego Restrepo <71931942+DiegoRestrepo02@users.noreply.github.com>
2026-03-14 09:25:22 -0500

    Ajustes finales, diseño, cart de compras

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,21 +1,47 @@
 import React, { useState } from "react";
 import { PropsProduct } from "../interfaces/propsProduct";
 
-export const ProductCard = (infoProduct: PropsProduct) => {
+interface listaProducts {
+    infoProducts: PropsProduct[]
+}
 
-    return(
+export const ProductCard = (infoProducts: listaProducts) => {
+    const [cart, setCart] = useState<number[]>([]);
+
+    const addToCart = (id: number) => {
+        setCart(prevCart => [...prevCart, id]);
+    };
+
+    return (
         <>
-        <article className="ProductCard">
-            <div className="imageContainer">
-                <img src={infoProduct.image} className="coverImage"/>
+            <div className="row">
+                <div className="col-sm-12 col-md-12 col-lg-12">
+                    <h1>Total productos a comprar: {cart.length}</h1>
+                    <h2>Id Productos:</h2>
+                    <ul>{cart.map((cartProducto, index) => (
+                        <li>
+                            {cartProducto}
+                        </li>
+                    ))}</ul>
+                </div>
             </div>
-            <div className="ProductInfo">
-                <strong className="ProductTitle">{infoProduct.title}</strong>
-                <span className="ProductPrice">{infoProduct.price}</span>
-                <span className="ProductDescription">{infoProduct.description}</span>
-                <span className="CategoryProduct">{infoProduct.category}</span>
+            <div className="row">
+                {
+                    infoProducts.infoProducts.map((product) => (
+                        <div className="col-sm-12 col-md-4 col-lg-4 my-3">
+                            <div className="card">
+                                <img src={product.image} className="rounded mx-auto d-block tamanioImagenProduct py-3" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{product.title}</h5>
+                                    <p className="card-text">{product.description}</p>
+                                    <p className="card-text fw-bold">${product.price}</p>
+                                    <button onClick={() => addToCart(product.id)} className="btn btn-primary text-center w-100">¡Comprar!</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
-        </article>
         </>
     )
 
--- src/components/ScreenProducts.tsx
+++ src/components/ScreenProducts.tsx
@@ -1,25 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ProductCard } from './ProductCard';
 import { getProducts } from '../helpers/getProducts';
 import { PropsProduct } from "../interfaces/propsProduct";
 
 export const ScreenProducts = () => {
-
     const [products, setProducts] = useState<PropsProduct[]>([]);
+    const [categories, setCategories] = useState<string[]>([]);
+    const [selectedCategory, setSelectedCategory] = useState<string>("0");
 
-    getProducts().then((resolveData) => {
-        if (resolveData != null) {
-            const productsApi: PropsProduct[] = resolveData;
-            setProducts(productsApi);
-        }
-    });
+    useEffect(() => {
+        getProducts().then((data) => {
+            if (data != null) {
+                setProducts(data);
+
+                const categoriasFinales: string[] = [];
+
+                data.forEach(d => {
+                    if (categoriasFinales.indexOf(d.category) == -1) {
+                        categoriasFinales.push(d.category);
+                    }
+                });
+
+                setCategories(categoriasFinales);
+                console.log(categoriasFinales);
+                console.log(categories);
+            }
+        })
+    }, []);
+
+    const productsMostrar = selectedCategory == "0" ? products : products.filter(p => p.category == selectedCategory);
 
     return (
         <>
+            <div className="row">
+                <div className="col-sm-12 col-md-12 col-lg-12">
+                    <div className="form-floating mt-5">
+                        <select className="form-select" id="floatingSelect" aria-label="Categorias" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+                            <option value="0">Todas</option>
+                            {categories.map((categoria, index) => (
+                                <option key={index} value={categoria}>
+                                    {categoria}
+                                </option>
+                            ))}
+                        </select>
+                        <label>Categorias</label>
+                    </div>
+                </div>
+            </div>
+            <div className="row mt-4">
+                <div className="col-sm-12 col-md-12 col-lg-12 text-center">
+                    <h1>{selectedCategory == "0" ? "Todas las categorías" : selectedCategory}</h1>
+                </div>
+            </div>
             {
-                products.map((product) => (
-                    <ProductCard id={product.id} title={product.title} price={product.price} description={product.description} image={product.image} category={product.category} />
-                ))
+                <ProductCard infoProducts={productsMostrar} />
             }
         </>
     )
